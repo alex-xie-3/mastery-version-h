f672ef55899d35e704350b8990314f8d09d9328b
Alex Xie <alex.xie@utexas.edu>
2025-04-08 17:33:35 -0500

    done

--- grind.c
+++ grind.c
@@ -40,8 +40,10 @@ grind_state_t grind(struct Parser* parser) {
                     }
                     free_grind_elem(&itr);
                     freed = true;
+                } else {
+                    prev = itr;
+                    itr = itr->next;
                 }
-                prev = itr;
             }
             if (!freed) {
                 return GRIND_BAD_FREE;
